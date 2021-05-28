4e086a5016534c8df9326179e85f1bf548334257
seooooooooook <80299241+seooooooooook@users.noreply.github.com>
2021-05-28 14:51:09 +0900

    2020.05.28

---
+++ src/components/basics/Loading.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Loading() {
+  return <div>지금은 로딩중입니다.</div>;
+}
+
+export default Loading;
--- src/components/pages/Reference.js
+++ src/components/pages/Reference.js
@@ -1,8 +1,10 @@
+import axios from "axios";
 import React from "react";
-// import Header from "../Header";
-// import Layout from "../Layout";
-// import Footer from "../Footer";
-// import WrapTitle from "../basics/WrapTitle";
+import Loading from "../basics/Loading";
+import Header from "../Header";
+import Layout from "../Layout";
+import Footer from "../Footer";
+import WrapTitle from "../basics/WrapTitle";
 // import { Link } from "react-router-dom";
 
 // function ReferText({ alpha, attr, desc }) {
@@ -47,48 +49,96 @@ import React from "react";
 
 // function Reference() {
 //   return (
-//     <div id="wrap" className="light">
-//       <Header info="none" />
-//       <Layout>
-//         <section id="referCont">
-//           <div className="container">
-//             <WrapTitle text={["HTML", "reference"]} />
-//             <div className="refer-cont">
-//               <div className="refer-table">
-//                 <h3>CSS Reference</h3>
-//                 <ul>
-//                   {referAttr.map((el) => (
-//                     <ReferText
-//                       key={el.attr}
-//                       alpha={el.alpha}
-//                       attr={el.attr}
-//                       desc={el.desc}
-//                     />
-//                   ))}
-//                 </ul>
-//               </div>
-//             </div>
+// <div id="wrap" className="light">
+//   <Header info="none" />
+//   <Layout>
+//     <section id="referCont">
+//       <div className="container">
+//         <WrapTitle text={["HTML", "reference"]} />
+//         <div className="refer-cont">
+//           <div className="refer-table">
+//             <h3>CSS Reference</h3>
+//             <ul>
+//               {referAttr.map((el) => (
+//                 <ReferText
+//                   key={el.attr}
+//                   alpha={el.alpha}
+//                   attr={el.attr}
+//                   desc={el.desc}
+//                 />
+//               ))}
+//             </ul>
 //           </div>
-//         </section>
-//       </Layout>
-//       <Footer />
-//     </div>
+//         </div>
+//       </div>
+//     </section>
+//   </Layout>
+//   <Footer />
+// </div>
 //   );
 // }
 
 class Reference extends React.Component {
   state = {
     isLoading: true,
+    refers: [],
+  };
+
+  getRefer = async () => {
+    const {
+      data: {
+        data: { htmlRefer },
+      },
+    } = await axios.get(
+      "https://seooooooooook.github.io/react3001/src/json/reference.json"
+    );
+    // console.log(htmlRefer);
+    this.setState({ htmlRefer, isLoading: false });
   };
 
   componentDidMount() {
     setTimeout(() => {
-      this.setState({ isLoading: false });
+      // this.setState({ isLoading: false });
+      this.getRefer();
     }, 3000);
   }
   render() {
     const { isLoading } = this.state;
-    return <div>{isLoading ? "로딩중" : "준비 완료"}</div>;
+    const { htmlRefer } = this.state;
+    return (
+      <div>
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <div id="wrap" className="light">
+            <Header info="none" />
+            <Layout>
+              <section id="referCont">
+                <div className="container">
+                  <WrapTitle text={["HTML", "reference"]} />
+                  <div className="refer-cont">
+                    <div className="refer-table">
+                      <h3>CSS Reference</h3>
+                      <ul>
+                        {htmlRefer.map((text) => (
+                          <Reference
+                            key={text.num}
+                            alpha={text.alpha}
+                            attr={text.attr}
+                            desc={text.desc}
+                          />
+                        ))}
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </section>
+            </Layout>
+            <Footer />
+          </div>
+        )}
+      </div>
+    );
   }
 }
 
